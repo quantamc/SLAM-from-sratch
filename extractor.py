e6253b7aa1848849e22e8999eced8d4b3209a7f4
quantamc <thabo_malete@outlook.com>
2019-01-19 23:47:10 +0200

    normalized coords

--- extractor.py
+++ extractor.py
@@ -2,15 +2,29 @@ import cv2
 import numpy as np
 
 from skimage.measure import ransac
+from skimage.transform import EssentialMatrixTransform
 from skimage.transform import FundamentalMatrixTransform
 
+#turn [[x,y]] -> [[x,y,1]]
+def add_ones(x):
+    return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
 
 class Extractor(object):
 
-    def __init__(self):
+    def __init__(self, K):
         self.orb = cv2.ORB_create(100)
         self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
         self.last = None
+        self.K = K
+        self.Kinv = np.linalg.inv(self.K)
+
+
+    def normalize(self, pts):
+        return np.dot(self.Kinv, add_ones(pts).T).T[:, 0:2]
+
+    def denormalize(self, pt):
+        ret = np.dot(self.K, np.array([pt[0], pt[1], 1.0]))
+        return int(round(ret[0])), int(round(ret[1]))
 
     def extract(self, img):
         # detection
@@ -33,11 +47,19 @@ class Extractor(object):
                     kp2 = self.last['kps'][m.trainIdx].pt
                     ret.append((kp1, kp2))
 
+        
+        
         # filter
 
         if len(ret) > 0:
             ret = np.array(ret)
+
+            # normalize and subtract to move to 0
+            ret[:, 0, :] = self.normalize(ret[:, 0, :])
+            ret[:, 1, :] = self.normalize(ret[:, 1, :])
+
             model, inliners = ransac((ret[:,0], ret[:,1]),
+                                #EssentialMatrixTransform,
                                 FundamentalMatrixTransform,
                                 min_samples=8, 
                                 residual_threshold=1, 
